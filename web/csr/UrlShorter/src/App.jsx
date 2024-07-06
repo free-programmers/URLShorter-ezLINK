06ee158b10ef777c4d24c2213f9cb1096bc8d6bf
cc-mehdi <kiarashjahangir6@gmail.com>
2024-07-07 00:30:34 +0330

    feat | frontend : add hero

--- web/csr/UrlShorter/src/App.jsx
+++ web/csr/UrlShorter/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header';
+import Hero from './components/Hero';
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
