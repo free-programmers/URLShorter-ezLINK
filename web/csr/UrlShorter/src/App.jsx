d4eac443d114a645ec04c216162e4fda7901f2c9
cc-mehdi <kiarashjahangir6@gmail.com>
2024-07-07 00:03:44 +0330

    feat | frontend : add header

---
+++ web/csr/UrlShorter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from './components/Header';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
