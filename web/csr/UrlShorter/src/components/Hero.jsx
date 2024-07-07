85af94cd0c61f433910165f4951d4635f87d49a6
ali sharifi <alisharifyofficial@gmail.com>
2024-07-07 20:47:50 +0330

    add font-awesome

--- web/csr/UrlShorter/src/components/Hero.jsx
+++ web/csr/UrlShorter/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-export default function Hero() {
+const Hero = ()=>{
     return (
         <>
             <div class="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
@@ -8,8 +8,8 @@ export default function Hero() {
                         <p class="mt-4 text-lg leading-8 text-gray-300">Stay in control of your links with advanced features for shortening and tracking.</p>
                         <div class="w-100">
                             <label for="email-address" class="sr-only">Your URL ...</label>
-                            <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-24 w-80 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="https://YOUR URL.com" />
-                            <button type="submit" class="ms-2 flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Do Short</button>
+                            <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-24 w-80 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="https://example.com" />
+                            <button type="submit" class="ms-2 flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Shorten</button>
                         </div>
                     </div>
                 </div>
@@ -20,3 +20,5 @@ export default function Hero() {
         </>
     )
 }
+
+export default Hero;
