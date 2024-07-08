f5d84a42fedb47307eb32f59ac8ed2cb9bad3ab7
cc-mehdi <kiarashjahangir6@gmail.com>
2024-07-08 19:20:09 +0330

    feat | frontend : add better navbar

--- web/csr/UrlShorter/src/components/Footer.jsx
+++ web/csr/UrlShorter/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <>
-            <footer class="flex items-center justify-center py-3 w-100 mt-3 bc-secondary rounded-xl" style={{ "box-shadow": "0 0 10px 1px var(--secondary)" }}>
+            <footer class="flex items-center justify-center py-3 w-100 mt-3 bc-secondary" style={{ "box-shadow": "0 0 10px 1px var(--secondary)" }}>
                 <div class="container-xxl">
                     <div class="mb-2 mb-md-0 text-center">
                         <span className="tc-white">
--- web/csr/UrlShorter/src/components/Hero.jsx
+++ web/csr/UrlShorter/src/components/Hero.jsx
@@ -2,11 +2,11 @@ const Hero = () => {
     return (
         <>
             <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32 rounded-xl" style={{ "box-shadow": "0 0 10px 1px var(--primary)" }}>
-                <div className="flex justify-center align-center">
+                <div className="flex justify-center items-center">
                     <div className="flex flex-col justify-center align-center">
-                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Smart and powerful short links</h2>
-                        <p className="mt-4 text-lg leading-8 text-gray-300">Stay in control of your links with advanced features for shortening and tracking.</p>
-                        <div className="w-100 mt-5">
+                        <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl text-center">Smart and powerful short links</h2>
+                        <p className="mt-4 text-lg leading-8 text-gray-300 text-center">Stay in control of your links with advanced features for shortening and tracking.</p>
+                        <div className="w-100 mt-5 text-center">
                             <label for="email-address" className="sr-only">Your URL ...</label>
                             <input id="email-address" name="email" type="email" autocomplete="email" required className="min-w-24 w-80 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="https://YOUR URL.com" />
                             <button type="submit" className="ms-2 flex-none rounded-md bc-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Shorten</button>
