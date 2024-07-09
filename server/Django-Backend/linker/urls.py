a3afff08fe057f98642a84d15b99c92c98a41c92
ali sharifi <87280202+alisharify7@users.noreply.github.com>
2024-07-09 22:57:51 +0330

    Update urls.py

--- server/Django-Backend/linker/urls.py
+++ server/Django-Backend/linker/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'linker'
+
 urlpatterns = [
     path('create/', views.create_alias_link, name="create-alias-link"),
     path('resolve/<str:link>/', views.resolve_alias_link, name="resolve-alias-link"),
     path('resolve/<str:link>/show/', views.show_alias_link_info, name="show-alias-link-info"),
-
 ]
