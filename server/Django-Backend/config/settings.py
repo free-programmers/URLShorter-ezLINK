cba1161ad9d7a38e4a416e0bb3eede6179197ee0
ali sharifi <alisharifyofficial@gmail.com>
2024-07-10 23:38:01 +0330

    Update settings.py

--- server/Django-Backend/config/settings.py
+++ server/Django-Backend/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a*^&@#g%56^i7(z^2hyq*l*$up3xwov#gdp$-g!rdx9g@^&c_0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
